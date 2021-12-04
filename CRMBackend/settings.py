6317878732f7ef88ff95f0234c056b6ffab44b18
yousra-ht <yousrahajtaieb@gmail.com>
2021-12-04 22:05:05 +0100

    04/12

--- CRMBackend/settings.py
+++ CRMBackend/settings.py
@@ -82,7 +82,7 @@ AUTH_USER_MODEL = "Auth.NewUser"
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=3),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
